7f987ba46532f956c785890f369dbd5fe9748657
sahraomar <sahraaliwrites@gmail.com>
2024-07-14 06:42:59 -0400

    last commit

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const express = require("express"); //import express module
+const express = require("express"); // import express module
 const app = express(); // create express application
 
 const PORT = 4000;
 
+// Set up the / route
+app.get("/", (req, res) => {
+  res.send("Hello, home!");
+});
+
+// Set up additional GET routes with HTML responses
 app.get("/Home", (req, res) => {
   res.send("<h1>Hello Home!</h1>");
 });
 
 app.get("/About", (req, res) => {
-  res.send("<h1>Hello,<b>H</b>!</h1>");
+  res.send("<h1>Welcome to About page!</h1>");
 });
-//response.send("<h1>Hello, <b>example</b>!</h1>")
 
 app.get("/Contact", (req, res) => {
-  res.send("Welcome to the Contact page!");
+  res.send("<h1>Welcome to the Contact page!</h1>");
 });
 
 app.get("/Newsletter", (req, res) => {
-  res.send("Check out our newsletter!");
+  res.send("<h1>Check out our newsletter!</h1>");
 });
 
-//start the server
+// Utilize request params
+app.get("/:page", (req, res) => {
+  const page = req.params.page;
+  res.send(`<h1>You are on the ${page} page!</h1>`);
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
